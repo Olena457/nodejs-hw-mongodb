68484a592ae2f34a8434fb81cc41c7f7b689960a
Olena457 <olenademianenko42@gmail.com>
2024-07-27 09:32:57 +0300

    corected json

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
 
-const PORT = Number(env('PORT', 8081));
+const PORT = Number(env('PORT', 3000));
 
 export const setupServer = () => {
   const app = express();
