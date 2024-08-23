2cac116bf662eb575326068ebf48115b94cf7dd3
Olena457 <olenademianenko42@gmail.com>
2024-08-23 10:56:48 +0300

    fixed all problem

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -16,36 +16,3 @@ export function validateBody(schema) {
     next();
   };
 }
-
-// export const validateBody = (schema) => async (req, res, next) => {
-//   try {
-//     await schema.validateAsync(req.body, {
-//       abortEarly: false,
-//     });
-//     next();
-//   } catch (err) {
-//     const error = createHttpError(400, 'Bad Request', {
-//       error: err.details,
-//     });
-//     next(error);
-//   }
-// };
-
-// export function validateBody(schema) {
-//   return async (req, res, next) => {
-//     try {
-//       await schema.validateAsync(req.body, { abortEarly: false });
-//       next();
-//     } catch (error) {
-//       console.log({ message: error.message });
-//       console.log({ details: error.details });
-
-//       next(
-//         createHttpError(
-//           400,
-//           error.details.map((err) => err.message).join(', '),
-//         ),
-//       );
-//     }
-//   };
-// }
