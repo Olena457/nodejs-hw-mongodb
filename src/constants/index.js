5d7ecdc4cc66315bddcd483a324f1daf0376515b
Olena457 <olenademianenko42@gmail.com>
2024-08-27 12:45:03 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,7 +8,7 @@ export const SORT_ORDER = {
 export const PHONE_REGEX = /^\+?[0-9]{10,15}$/;
 export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-export const ACCESS_TIME = 15 * 60 * 60 * 1000;
+export const ACCESS_TIME = 30 * 60 * 60 * 1000;
 export const REFRESH_TIME = 30 * 24 * 60 * 60 * 1000;
 
 export const JWT_SECRET = 'JWT_SECRET';
