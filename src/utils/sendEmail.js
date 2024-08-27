203069a62e59aadf2e94c5ff5216481ca609a4a0
Olena457 <olenademianenko42@gmail.com>
2024-08-27 20:42:03 +0300

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -5,7 +5,7 @@ import { env } from '../utils/env.js';
 
 const transport = nodemailer.createTransport({
   host: env(EMAIL_VARS.SMTP_HOST),
-  port: env(EMAIL_VARS.SMTP_PORT),
+  port: Number(env(EMAIL_VARS.SMTP_PORT)),
   auth: {
     user: env(EMAIL_VARS.SMTP_USER),
     pass: env(EMAIL_VARS.SMTP_PASS),
