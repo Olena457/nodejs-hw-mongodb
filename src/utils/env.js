4e71f601f4368feb0be55619e6641931fe92bb85
Olena457 <olenademianenko42@gmail.com>
2024-08-28 13:52:31 +0300

    Update env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,22 +1,22 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export function env(name, defaultValue) {
-  const value = process.env[name];
-
-  if (value) return value;
-
-  if (defaultValue != undefined) return defaultValue;
-
-  throw new Error(`Missing: process.env['${name}'].`);
-}
 // import dotenv from 'dotenv';
+
 // dotenv.config();
 
-// export const env = (name, defaultValue) => {
+// export function env(name, defaultValue) {
 //   const value = process.env[name];
+
 //   if (value) return value;
-//   if (defaultValue) return defaultValue;
-//   throw new Error(`${name} variable doesn't exist`);
-// };
+
+//   if (defaultValue != undefined) return defaultValue;
+
+//   throw new Error(`Missing: process.env['${name}'].`);
+// }
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const env = (name, defaultValue) => {
+  const value = process.env[name];
+  if (value) return value;
+  if (defaultValue) return defaultValue;
+  throw new Error(`${name} variable doesn't exist`);
+};
